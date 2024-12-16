49b496d4a2c93d4172d65712fd981ecacc9edd71
duywillbe <duyducmkt@gmail.com>
2024-12-16 16:23:15 +0700

    tach Weeks thanh component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,8 @@
 import datalesson from './assets/databaihocIELTS/databaihocielts.json';
 import React from 'react';
 import { useState } from 'react';
-import ielts_coach from './assets/images/IELTS Coach.svg';
 import target_tiny from './assets/icon/target-tiny.svg';
 import avt_lesson from './assets/icon/avt_lesson.svg';
-import microphone from './assets/images/microphone.svg';
-import home_line from './assets/icon/home-line.svg';
-import dethi from './assets/icon/dethi.svg';
-import phongthi from './assets/icon/phongthi.svg';
-import chart from './assets/icon/bar-chart-square-02.svg';
 import SidebarLeft from './components/sidebarLeft';
 import Weeks from './components/weeks';
 
@@ -72,57 +66,11 @@ function App() {
               <div>
                 <div className="pt-[18px] pb-8 px-8 mt-[32px] rounded-3xl bg-[#ebedee] max-w-[760px]">
                   {/*--------chủ đề --------- */}
-                  {phase.tasks_sections.map((section) => (
-                    <div>
-                      <div>
-                        <img src={target_tiny} alt="mySvgImage" className="mt-[12px]" />
-                      </div>
-                      {/*  ---------bài học----*/}
-                      <div className="text-[#101828] font-semibold text-[18px] mt-[12px]">{section.name}</div>
-                      <div className="text-[#475467] text-[14px]">
-                        {section.description}
-                        <p
-                          className="text-[#2525e2] mt-[12px]"
-                          onClick={() => {
-                            handleHideShow();
-                          }}
-                        >
-                          {/* toán tử ba ngôi  */}
-                          {`${isShowDetail ? 'Ẩn' : 'Hiện'} danh sách`}
-                        </p>
-                      </div>
-                      <div className=" mt-[24] rounded-3xl">
-                        <div className="flex">
-                          {/* --backgound từng bài học--- */}
-                          {isShowDetail && (
-                            <div className="w-full">
-                              {section.tasks.map((task) => (
-                                <div className="bg-[white] flex p-8 mt-[14px] rounded-3xl font-semibold  justify-between ">
-                                  <div className="flex">
-                                    <div>
-                                      <img src={avt_lesson} alt="" />
-                                    </div>
-                                    <div className="ml-[14px] ">
-                                      <div className="w-[300px]">
-                                        <p className="brake-works">{task.name}</p>
-                                      </div>
-                                      <div>
-                                        <p className="text-[12px] font-extralight">{task.estimate_time} min</p>
-                                      </div>
-                                    </div>
-                                  </div>
-
-                                  <button className="  px-4 bg-[#1890FF] text-[white] rounded-[12px] font-semibold">Bắt đầu</button>
-                                </div>
-                              ))}
-                            </div>
-                          )}
-                        </div>
-                      </div>
-                    </div>
+                  {phase.tasks_sections.map((section, index) => (
+                    /*----------- Weeks -----------*/
+                    <Weeks key={index} section={section} a={'ssss'} />
                   ))}
                 </div>
-                handleHideShow
               </div>
             </div>
           ))}
--- src/components/weeks.js
+++ src/components/weeks.js
@@ -1,9 +1,71 @@
 import datalesson from '../assets/databaihocIELTS/databaihocielts.json';
 import React from 'react';
+import { useState } from 'react';
+import target_tiny from '../assets/icon/target-tiny.svg';
+import avt_lesson from '../assets/icon/avt_lesson.svg';
 
-const Weeks = () => {
-  return;
-  <div></div>;
+const Weeks = ({ section }) => {
+  const [phases] = useState(datalesson.body.phases);
+  const [isShowDetail, setIsShowdetail] = useState(true);
+  const handleHideShow = () => {
+    let currentStatus;
+    if (isShowDetail === true) currentStatus = false;
+    if (isShowDetail === false) currentStatus = true;
+
+    setIsShowdetail(!isShowDetail);
+  };
+
+  return (
+    <div>
+      <div className="bg-[#48c9b0]">
+        <div>
+          <img src={target_tiny} alt="mySvgImage" className="mt-[12px]" />
+        </div>
+        {/*  ---------bài học----*/}
+        <div className="text-[#101828] font-semibold text-[18px] mt-[12px]">{section.name}</div>
+        <div className="text-[#475467] text-[14px]">
+          {section.description}
+          <p
+            className="text-[#2525e2] mt-[12px]"
+            onClick={() => {
+              handleHideShow();
+            }}
+          >
+            {/* toán tử ba ngôi  */}
+            {`${isShowDetail ? 'Ẩn' : 'Hiện'} danh sách`}
+          </p>
+        </div>
+        <div className=" mt-[24] rounded-3xl">
+          <div className="flex">
+            {/* --backgound từng bài học--- */}
+            {isShowDetail && (
+              <div className="w-full">
+                {section.tasks.map((task) => (
+                  <div className="bg-[white] flex p-8 mt-[14px] rounded-3xl font-semibold  justify-between ">
+                    <div className="flex">
+                      <div>
+                        <img src={avt_lesson} alt="" />
+                      </div>
+                      <div className="ml-[14px] ">
+                        <div className="w-[300px]">
+                          <p className="brake-works">{task.name}</p>
+                        </div>
+                        <div>
+                          <p className="text-[12px] font-extralight">{task.estimate_time} min</p>
+                        </div>
+                      </div>
+                    </div>
+
+                    <button className="  px-4 bg-[#1890FF] text-[white] rounded-[12px] font-semibold">Bắt đầu</button>
+                  </div>
+                ))}
+              </div>
+            )}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Weeks;
